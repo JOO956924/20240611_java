04cf545eb5e5cbbb52b1c17892702a52527234a4
JOO956924 <wndbs000@gmail.com>
2024-06-11 12:26:09 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// class
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+  // 멤버 변수
+  int num1 = 10;
+  int num2 = 30;
+  int num3 = num1 + num2;
+// 클래스없이 멤버변수 x
+  void done() {
+    System.out.println(num3);
+    if (num1 < num2) {
+      num3 = num1;
+    } else {
+      num3 = num2;
     }
+  }
+
+  public static void main(String[] args) {
+    Main main = new Main();
+    main.done();
+  }
+}
+/*
+클래스 선언할 경우
+1) 멤버변수와 메서드 없어도 클래스 선언가능.
+2) ★멤버변수와 메서드를 정의하려고 하면 반드시 클래스를 선언
+3) class 앞에 public이 붙을 경우 반드시 파일명과 일치하는 클래스에 붙일것
+   (파일명과 클래스이름이 같아야 퍼블릭(접근제어자) 부여가능).
+4) 클래스를 사용하려면 인스턴스를로 만들어야 한다.
+5) 클래스는 new(생성연산자)를 통해 인스턴스를 생성할 수 있다.
+*/
+class Animal {
+
 }
---
+++ src/MainAnother.java
@@ -0,0 +1,10 @@
+public class MainAnother {
+  public static void main(String[] args) {
+    /*Main main = new Main();
+    main.done();*/
+    Animal animal = new Animal();
+    Animal animal2 = new Animal();
+    System.out.println(animal2);
+    //인스턴스 = 복제본//
+  }
+}
