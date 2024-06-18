d9635b8e07ec899ca3b9a161de2a2dac272302aa
JOO956924 <wndbs000@gmail.com>
2024-06-18 16:35:03 +0900

    init

---
+++ src/p03_method/Ex07condition.java
@@ -0,0 +1,37 @@
+package p03_method;
+
+import java.util.Scanner;
+
+public class Ex07condition {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("숫자 입력:");
+    int input = sc.nextInt();
+    System.out.println(input);
+    String result = "";
+    if(input >= 90) {
+      result = "A";
+    } else if (input >= 80) {
+      result = "B";
+    } else if (input >= 70) {
+      result = "C";
+    } else if (input >= 60) {
+      result = "D";
+    } else {
+      result = "F";
+    }
+    if (input == 100) {
+      result = "A+";
+    } else if ( input < 60 ) {
+      result = "F";
+    } else if ( (input%10) >=7) {
+      result = result+"+";
+    } else if ( (input%10) >= 4) {
+      result = result+"0";
+    } else if ( (input%10) >= 0) {
+      result = result+"-";
+    }
+
+    System.out.println(result + "학점입니다");
+  }
+}
