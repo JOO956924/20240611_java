138c21636c24354cd2c70ec202040264f42c224c
JOO956924 <wndbs000@gmail.com>
2024-06-21 14:34:33 +0900

    init

--- src/p05_Inherit/Ex05Override.java
+++ src/p05_Inherit/Ex05Override.java
@@ -1,7 +1,42 @@
 package p05_Inherit;
 
+import java.util.Objects;
+
 public class Ex05Override {
   public static void main(String[] args) {
+    Data d1 = new Data(1);
+    Data d2 = new Data(1);
+    System.out.println(d1);
+    System.out.println(d2);
+    if (d1.equals(d2)) {
+      System.out.println("값이 같다");
+    } else if (d1!=d2) {
+      System.out.println("값이 다르다");
+    }
+  }
+}
 
+class Data {
+  int value;
+
+  public Data(int value) {
+    this.value = value;
+
+  }
+  public String toString() {return "value:" + value;}
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Data) {
+      Data d = (Data) obj;
+      return value == d.value;
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    //hashcode의 값이 동일할때
+    return Objects.hash(value);
   }
 }
