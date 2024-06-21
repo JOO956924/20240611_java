ab48260d624c7c2ca32d18935dd9c04ebf663525
JOO956924 <wndbs000@gmail.com>
2024-06-21 16:06:40 +0900

    init

---
+++ src/p05_Inherit/Ex08InterfaceCasting.java
@@ -0,0 +1,45 @@
+package p05_Inherit;
+
+import common.Utils;
+
+public class Ex08InterfaceCasting {
+  public static void main(String[] args) {
+    Mammals[] mammals = {new Dog(), new Cat(), new Bat()};
+    Birds[] birds = {new Chicken(), new Eagle(), new Parrot()};
+
+    Flyable[] flyables = {new Bat(), new Eagle(), new Parrot()};
+    IGround[] iGrounds = {new Dog(), new Cat(), new Chicken()};
+    Object obj = 10;
+    Utils.typeOf(obj);
+  }
+}
+
+interface Flyable {
+}
+
+interface IGround {
+}
+
+class Birds {
+}
+
+class Parrot extends Birds implements Flyable {
+}
+
+class Eagle extends Birds implements Flyable {
+}
+
+class Chicken extends Birds implements IGround {
+}
+
+class Mammals {
+}
+
+class Dog extends Mammals implements IGround {
+}
+
+class Cat extends Mammals implements IGround {
+}
+
+class Bat extends Mammals implements Flyable {
+}
