0382ff02aa4a6c2a07ba5ac6325010020e773878
JOO956924 <wndbs000@gmail.com>
2024-06-21 16:30:20 +0900

    init

--- src/p05_Inherit/Ex09Abstract.java
+++ src/p05_Inherit/Ex09Abstract.java
@@ -1,10 +1,16 @@
 package p05_Inherit;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Ex09Abstract {
   public static void main(String[] args) {
     // 스스로 인스턴스 생성 불가(추상크래스,interface)
     // Abstract abs = new Abstract();
     // Interface inter = new Interface();
+    new MyFrame();
   }
 }
 // 공통(표준)으로 사용위해 만듦, 인스턴스 생성 불가
@@ -14,3 +20,22 @@ abstract class Abstract {
   abstract void special();
 }
 interface Interface {}
+
+class MyFrame extends JFrame {
+  public MyFrame() throws HeadlessException {
+    setSize(300,200);
+    setTitle("My Window");
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+    setLayout(new FlowLayout());
+    JButton jButton = new JButton("확인");
+    jButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        System.out.println("hello ");
+      }
+    });
+    add(jButton);
+    setVisible(true);
+  }
+}
