c4a7bb8fabde185b1617121633a12bc6533c1c94
JOO956924 <wndbs000@gmail.com>
2024-06-28 14:20:28 +0900

    init

--- src/p10_Stream/Ex01ArrayCollection.java
+++ src/p10_Stream/Ex01ArrayCollection.java
@@ -1,6 +1,8 @@
 package p10_Stream;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -9,7 +11,13 @@ public class Ex01ArrayCollection {
   public static void main(String[] args) {
     String[] stArr1 = {"abc", "def", "ghi"};
     //배열을 Stream으로 변경
-    //Stream<String> tmpStream = Arrays.stream(stArr1);
+    Stream<String> tmpStream = Arrays.stream(stArr1);
+    tmpStream.forEach(new Consumer<String>() {
+      @Override
+      public void accept(String s) {
+        System.out.println(s);
+      }
+    });
     String[] stArr2 = {"ABC", "DEF", "GHI"};
     //2개의 배열을 하나의 Stream으로 변경
     Stream<String[]> strArrStream = Stream.of(stArr1, stArr2);
@@ -36,5 +44,20 @@ public class Ex01ArrayCollection {
             System.out.println(s);
           }
         });
+
+    List<String> list = new ArrayList<>();
+    list.add("손흥민");
+    list.add("황희찬");
+    list.add("황인범");
+    list.add("김민재");
+    //Stream<String> nameStream = list.stream();
+    // ForkJoinPool관리방식을 사용해서 복잡하던 스레드 관리방식을 Fork와 Join을 통해서
+    // 분할 정보(Divide and Conquer)기법으로 처리해준다.
+    Stream<String> nameStream = list.parallelStream();
+    nameStream.forEach(s -> {
+      System.out.println(s);
+      System.out.println(s.toLowerCase());
+    });
   }
 }
+
