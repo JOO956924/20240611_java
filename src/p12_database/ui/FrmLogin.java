c62f79933615474af49c401e6e02d79557628226
JOO956924 <wndbs000@gmail.com>
2024-07-01 16:37:44 +0900

    init

--- src/p12_database/ui/FrmLogin.java
+++ src/p12_database/ui/FrmLogin.java
@@ -1,4 +1,22 @@
 package p12_database.ui;
 
-public class FrmLogin {
+public class FrmLogin extends FrmBasic {
+  public FrmLogin(String title, int width, int height) {
+    super(title, width, height);
+  }
+
+  @Override
+  public void init() {
+
+  }
+
+  @Override
+  public void arrange() {
+
+  }
+
+  @Override
+  public void inflate() {
+    super.inflate();
+  }
 }
