88143d64b7fd99f2397f803814f45e4c62b45baf
JOO956924 <wndbs000@gmail.com>
2024-07-01 15:13:08 +0900

    init

---
+++ src/p12_database/DAOSet.java
@@ -0,0 +1,28 @@
+package p12_database;
+
+import java.sql.*;
+
+public class DAOSet {
+  private Connection conn;
+  private PreparedStatement pstmt;
+  private Statement stmt;
+  private ResultSet rs;
+
+  public Connection connectDB() throws SQLException {
+    //String driver = "jdbc:mariadb://127.0.0.1:3306/db7"; // maria db
+    String driver = "jdbc:oracle:thin:@localhost:1521:xe";
+    String user = "db7", pass = "1234";
+    conn = DriverManager.getConnection(driver, user, pass);
+    return conn;
+  }
+  public void closeDB() {
+    try {
+      if (rs != null) rs.close();
+      if (stmt != null) stmt.close();
+      if (pstmt != null) pstmt.close();
+      if (conn != null) conn.close();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ src/p12_database/DAOSetTest.java
@@ -0,0 +1,18 @@
+package p12_database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DAOSetTest {
+  public static void main(String[] args) {
+    DAOSet daoSet = new DAOSet();
+    try {
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+      Connection conn = daoSet.connectDB();
+    } catch (ClassNotFoundException e) {
+      System.out.println("드라이버로딩 오류:" + e.getMessage());
+    } catch (SQLException e) {
+      System.out.println("접속 오류:" + e.getMessage());
+    }
+  }
+}
