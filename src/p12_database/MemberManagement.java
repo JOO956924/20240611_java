910da87d167dc74aa3e0530a7a0df20df239c26d
JOO956924 <wndbs000@gmail.com>
2024-07-02 09:40:08 +0900

    init

---
+++ src/p12_database/MemberManagement.java
@@ -0,0 +1,9 @@
+package p12_database;
+
+import p12_database.ui.FrmLogin;
+
+public class MemberManagement {
+  public static void main(String[] args) {
+    new FrmLogin("Welcom Login",200,150);
+  }
+}
--- src/p12_database/ui/FrmLogin.java
+++ src/p12_database/ui/FrmLogin.java
@@ -1,24 +1,46 @@
 package p12_database.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class FrmLogin extends FrmBasic {
   private JLabel lbLogin, lbID, lbPass;
   private JTextField tfId;
   private JPasswordField pfPass;
   private JButton btnLogin, btnJoin, btnClose;
+  private JPanel pnlCenter, pnlSouth;
+
   public FrmLogin(String title, int width, int height) {
     super(title, width, height);
   }
 
   @Override
   public void init() {
+    lbLogin = new JLabel("Login");
+    lbID = new JLabel("Id");
+    lbPass = new JLabel("Pass");
+    tfId = new JTextField(20);
+    pfPass = new JPasswordField(20);
+    btnLogin = new JButton("Login");
+    btnJoin = new JButton("Join");
+    btnClose = new JButton("Close");
+    pnlCenter = new JPanel(new GridLayout(2,2));
+    pnlSouth = new JPanel();
 
   }
 
   @Override
   public void arrange() {
-
+    add(lbLogin,"North");
+    pnlCenter.add(lbID);
+    pnlCenter.add(tfId);
+    pnlCenter.add(lbPass);
+    pnlCenter.add(pfPass);
+    add(pnlCenter,"Center");
+    pnlSouth.add(btnLogin);
+    pnlSouth.add(btnJoin);
+    pnlSouth.add(btnClose);
+    add(pnlSouth,"South");
   }
 
   @Override
